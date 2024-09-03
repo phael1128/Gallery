fab8fde00973a91ccb5e41fd78d30696a22b0cda
phael <phael.dev@kidsnote.com>
2024-09-03 10:26:16 +0900

    hilt Application 주입

--- app/src/main/java/com/example/gallery/GalleryApplication.kt
+++ app/src/main/java/com/example/gallery/GalleryApplication.kt
@@ -1,4 +1,7 @@
 package com.example.gallery
 
-class GalleryApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GalleryApplication: Application() {}

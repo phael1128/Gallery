65604db32da5be7633e6cdaacdbd2e585ba8c3ce
phael <phael.dev@kidsnote.com>
2024-09-10 17:42:39 +0900

    ViewHolder 정사각형을 만들기 위해 View 추가

---
+++ app/src/main/java/com/example/gallery/SquareRelativeLayout.kt
@@ -0,0 +1,28 @@
+package com.example.gallery
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+
+class SquareRelativeLayout : RelativeLayout {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}

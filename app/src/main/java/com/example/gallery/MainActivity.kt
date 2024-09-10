75c5274a6e4d2a6c7d1085d06cd5fd0b906fa457
phael <phael.dev@kidsnote.com>
2024-09-10 17:44:12 +0900

    Storage 권한 체크 로직 추가 및 RecyclerView 추가

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -1,23 +1,98 @@
 package com.example.gallery
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.gallery.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private val mediaItemListAdapter: GalleryAdapter by lazy {
+        GalleryAdapter(
+            viewModel.getMediaItemList(),
+            Glide.with(this@MainActivity)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        with (binding) {
+            rvImages.adapter = mediaItemListAdapter
+            rvImages.layoutManager = GridLayoutManager(this@MainActivity, 3)
+        }
+
+        onUiCollect()
+        requestPermission()
+    }
+
+    private fun onUiCollect() {
+        viewModel.notifyMediaItem.observe(this@MainActivity) {
+            mediaItemListAdapter.notifyItemRangeChanged(it.startIndex, it.endIndex)
+        }
+    }
+
+    private fun requestPermission() {
+        if (!checkStoragePermission()) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                REQUEST_CODE_IMAGE_PERMISSIONS
+            )
+        } else {
+            Log.d("phael", "권한 부여 확인")
+            viewModel.getImageMediaList()
+        }
+    }
+
+    private fun checkStoragePermission() : Boolean {
+        return ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_IMAGE_PERMISSIONS) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                viewModel.getImageMediaList()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_IMAGE_PERMISSIONS = 1001
     }
 }
